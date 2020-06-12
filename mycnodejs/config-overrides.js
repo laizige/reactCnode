261cc6258bc31090032c5753b1bceba8768b993b
crl201203 <crl201203@163.com>
2020-06-12 18:35:38 +0800

    根据antd官网，修改配置

--- mycnodejs/config-overrides.js
+++ mycnodejs/config-overrides.js
@@ -1,5 +1,21 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-  return config;
+
+// 参考官网的配置
+// https://ant.design/docs/react/use-with-create-react-app-cn
+
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            modifyVars: { '@primary-color': '#1DA57A' },
+            javascriptEnabled: true,
+          },
+        },
+      },
+    },
+  ],
 };
