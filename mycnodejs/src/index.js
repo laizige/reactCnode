4223f1c2cfd9474d77eab9b3650e72b86de2a66e
crl201203 <crl201203@163.com>
2020-06-12 18:03:01 +0800

    初始化项目，建立路由

---
+++ mycnodejs/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+// 入口文件 引入路由 ,然后再去 App.js 中引入 router
+import {HashRouter} from 'react-router-dom'
+
+// import {BrowserRouter} from "react-router-dom"   该写法 会去掉路由中的#，使用嵌套方法类似HashRouter
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(
+  <HashRouter>
+     <App />
+  </HashRouter>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ mycnodejs/src/router/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+
+// 引入首页模块
+import Index from '../view/index/index'
+import Book from '../view/book/index'
+import About from '../view/about/index'
+import User from '../view/user/index'
+import Details from '../view/details/index'
+
+class RouterIndex extends Component{
+  render(){
+    return(
+      <Switch>
+        {/* 首页这句是重定向 */}
+        <Route path="/"  exact
+          render={()=>(
+            <Redirect to ="/index" />
+          
+        )}/>
+
+        <Route path="/index" component={Index} />
+        <Route path="/book" component={Book} />
+        <Route path="/about" component={About} />
+        <Route path="/user" component={User} />
+        <Route path="/details" component={Details} />
+      
+      </Switch>
+    )
+    
+  }
+}
+
+export default RouterIndex
